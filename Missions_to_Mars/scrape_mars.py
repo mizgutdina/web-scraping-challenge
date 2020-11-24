e350cb1f5f025772ce07964cf830b5b95037a792
medinai <64745369+mizgutdina@users.noreply.github.com>
2020-11-23 20:29:38 -0600

    Adds more for scrape file

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -12,7 +12,7 @@ def init_browser():
 def scrape_info():
     browser = init_browser()
 
-    #Visit NASA Mars News website
+    ###Visit NASA Mars News website
     url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
     browser.visit(url)
 
@@ -38,10 +38,58 @@ def scrape_info():
         "news_p":news_p
     }
 
+    ###Featured Image
+    url_two = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url_two)
+
+    browser.links.find_by_partial_text('FULL IMAGE').click()
+
+    browser.links.find_by_partial_text('more info').click()
+
+    html = browser.html
+    soup1 = BeautifulSoup(html, 'html.parser')
+
+    #Get the link to featured image [image=new soup]
+    image = soup1.select_one('figure.lede a img').get('src')
+    image
+    featured_image_url = f'https://www.jpl.nasa.gov/{image}'
+    featured_image_url
+
+    featured_image_data = {
+        "featured_image_url":featured_image_url
+    }
+
+    ###Mars Facts
+    url_three = 'https://space-facts.com/mars/'
+
+    #Get tables using pandas
+    tables = pd.read_html(url_three)
+    #tables
+
+    df = tables[0]
+    #df.head(10)
+
+    facts_df = df.rename(columns={0: "Description", 1: "Mars"})
+    #facts_df
+
+    html_table = facts_df.to_html()
+    #html_table
+
+    html_table_string = html_table.replace('\n', '')
+    #html_table_string
+
+    ###Mars Hemispheres  - unfinished part
+    hemisphere_image_urls = [
+    {"title": "Valles Marineris Hemisphere", "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif/full.jpg"},
+    {"title": "Cerberus Hemisphere", "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg"},
+    {"title": "Schiaparelli Hemisphere", "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif/full.jpg"},
+    {"title": "Syrtis Major Hemisphere", "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif/full.jpg"},
+    ]
+
 
 
     # Close the browser after scraping
     browser.quit()
 
     # Return results
-    return news_data
+    return news_data, featured_image_data, html_table_string, hemisphere_image_urls
